51261f3823e67c798e026b1695dc5d301c8b8613
Kerem Özakça <keremmozakca@gmail.com>
2025-05-05 14:59:11 +0300

    PArt4 modified

--- Assets/Kerem/Scripts/ToyInteractor.cs
+++ Assets/Kerem/Scripts/ToyInteractor.cs
@@ -7,21 +7,38 @@ public class ToyInteractor : MonoBehaviour
 {
     [SerializeField] Button repairButton;
     [SerializeField] GameObject panel;
+    private Toy toy;
+    private void Awake()
+    {
+        toy = GetComponent<Toy>();
+    }
+
+    public Button GetRepairButton()
+    {
+        return repairButton;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Dog"))
+        if (!toy.IsFixed())
         {
-            repairButton.gameObject.SetActive(true);
+            if (collision.gameObject.CompareTag("Adult"))
+            {
+                repairButton.gameObject.SetActive(true);
+            }
+            else
+            {
+
+            }
         }
         else
         {
-
+            repairButton.gameObject.SetActive(false);
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Dog"))
+        if (collision.gameObject.CompareTag("Adult"))
         {
             repairButton.gameObject.SetActive(false);
         }
@@ -36,6 +53,13 @@ public class ToyInteractor : MonoBehaviour
         Debug.Log("Buton Çalıştı!");
         panel.SetActive(true);
 
+        DropHandler handler = panel.transform.Find("dropZone").GetComponent<DropHandler>();
+
+        if (handler != null)
+        {
+            handler.SetCurrentToy(this.gameObject); // BU SATIR EKLENDİ!
+            handler.ResetTools(); // Eğer işlem sıfırlamak istiyorsan
+        }
     }
 
 }
